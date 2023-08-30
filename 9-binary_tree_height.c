e431c9e10b615b985b77b14831006d8c36c9ef42
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-08-30 14:38:37 +0200

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
   if (tree == NULL)
     return (0);
 
-  l = binary_tree_height(tree->left) ? l + 1 : 0;
-  r = binary_tree_height(tree->right) ? r + 1 : 0;
+  l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+  r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
   return ((l > r) ? l : r);
 }
