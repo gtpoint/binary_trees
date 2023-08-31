526e7f76d707753051772a7142c9fb43e87780d9
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-08-31 11:40:46 +0200

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  if (tree){
-    return (tree->parent ? 1 + binary_tree_depth(tree->parent) : 0);
-  }
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
