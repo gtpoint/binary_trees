1edb992ec6b0e4e1041516ec3241c43142bfd560
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-08-31 17:18:00 +0200

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
   if (tree->left == NULL && tree->right == NULL)
-        return (0);
+        return (1);
 
   if ((tree->left) && (tree->right))
         return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
